014c75d6ca788f6a204c91658807df59cccd7091
bprabhati <prabhati.mili@gmail.com>
2025-06-05 12:47:42 +0530

    modified myapp/urls.py and myapp/views.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,8 @@ from myapp import views
 urlpatterns = [
     path('home/', views.home_func,name='home_name'),
     path('register/',views.register_view,name='register_name'),
-    path('login/',views.login_view,name="login_name"),
-    path('logout/',views,name="logout_name"),
+    path('login/',views.login_view, name="login_name"),
+    path('logout/',views.logout_view, name="logout_name"),
+    path('external-joke/', views.get_joke_from_external_api, name='external_joke'),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from myapp.forms import LoginForm, RegisterForm
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.decorators import login_required
-
+import requests
 
 # Create your views here.
 @login_required
@@ -47,5 +47,18 @@ def login_view(request):
             return render(request, 'login.html', {"form":form})
 def logout_view(request):
      logout(request) 
-     return JsonResponse({"msg":"user logged out successfully"})      
+     return JsonResponse({"msg":"user logged out successfully"})  
+ 
+def get_joke_from_external_api(request):
+    url = "https://official-joke-api.appspot.com/random_joke"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return JsonResponse({"joke": data})
+        else:
+            return JsonResponse({"error": "Failed to fetch joke"}, status=response.status_code)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+   
             
